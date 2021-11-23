57a5486104481a1de457803822b58946e436caba
irsyadazka <16520096@mahasiswa.itb.ac.id>
2021-11-23 13:41:29 +0700

    ganti print jd return

--- operatorsplit.py
+++ operatorsplit.py
@@ -7,7 +7,7 @@ def splitOperator(filename):
 
     # split the target string on the occurance of one or more whitespace characters
     output = re.split(r'\s+', input)
-    print(output)
+    #print(output)
 
     operator = ['=', '!=', '==', '>=', '<=', '<', '>', ':', ',', '/', '-', r'\+', r'\*', r'\*\*', r'\'', r'\"', r'\'\'\'', r'\(', r'\)']
     
@@ -20,13 +20,13 @@ def splitOperator(filename):
             for splitted in elmt:
                 temp.append(splitted) 
         output = temp
-    print(output)
+    #print(output)
 
     # remove the blank characters from the output
     for statement in output:
         if statement == '':
             output.remove(statement)
 
-    print(output)
+    return output
 
-splitOperator("tes.txt")
+print(splitOperator("tes.txt"))
